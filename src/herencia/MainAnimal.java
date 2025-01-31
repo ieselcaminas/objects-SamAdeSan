2ae8ccebdd08698b84a7476fcf4691397a1c5003
samadesan <samadesan@alu.edu.gva.es>
2025-01-31 13:08:11 +0100

    animal

---
+++ src/herencia/MainAnimal.java
@@ -0,0 +1,36 @@
+package herencia;
+
+public class MainAnimal {
+    public static void main(String[] args) {
+//        Animal animal = new Animal("Samuel");
+//        System.out.println(animal.getNombre());
+//        animal.comunicarse();
+
+        Perro perro = new Perro("Rufo");
+        perro.comunicarse();
+        perro.traerZapatillas();
+        perro.mamar();
+
+        Gato gato = new Gato("Mishifu");
+        gato.comunicarse();
+        gato.perseguirUnOvillo();
+        gato.mamar();
+        gato.respirar();
+
+        Tiburon tiburon = new Tiburon("Tiburón");
+        tiburon.comunicarse();
+        tiburon.respirar();
+
+        Entrenador pepe = new Entrenador("Juan");
+
+        pepe.entrenar(gato);
+        pepe.entrenar(tiburon);
+
+        pepe.dejarEntrar(perro);
+        pepe.dejarEntrar(gato);
+
+        perro.setNombre("Pedro");
+        System.out.println(perro);
+
+    }
+}
